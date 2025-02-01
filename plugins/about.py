2565478a93b42f3adda132d7d692f92591c03bad
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-01 21:13:54 +0000

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','7857832326:AAEelUaX96QkzWzj-NmPXP418LF_MqUbNPA')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
@@ -10,4 +10,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"Total User:- {total_user()}\nBot :-[Ultra renamer bot](http://t.me/Ultra_renamer_bot)\nCreateur** :- @Kingcey**\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway\nFichier Total renommé :-{total_rename}\nPoids Total :- {humanbytes(int(total_size))} ",quote=True)
